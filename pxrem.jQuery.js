d5f6a3f552a91ab66ec9c8f2e3344e776e114b75
John Leonard <thejohnleonard@gmail.com>
2014-09-30 15:29:42 -0400

    Update and rename pxem.jQuery.js to pxrem.jQuery.js

--- pxrem.jQuery.js
+++ pxrem.jQuery.js
@@ -9,15 +9,15 @@
  * Usage Example: $(myPixelValue).toEm(); or $(myEmValue).toPx();
 --------------------------------------------------------------------*/
 
-$.fn.toEm = function(settings){
+$.fn.toRem = function(settings){
 	settings = jQuery.extend({
 		scope: 'body'
 	}, settings);
 	var that = parseInt(this[0],10),
-		scopeTest = jQuery('<div style="display: none; font-size: 1em; margin: 0; padding:0; height: auto; line-height: 1; border:0;">&nbsp;</div>').appendTo(settings.scope),
+		scopeTest = jQuery('<div style="display: none; font-size: 1rem; margin: 0; padding:0; height: auto; line-height: 1; border:0;">&nbsp;</div>').appendTo(settings.scope),
 		scopeVal = scopeTest.height();
 	scopeTest.remove();
-	return (that / scopeVal).toFixed(8) + 'em';
+	return (that / scopeVal).toFixed(8) + 'rem';
 };
 
 
@@ -26,7 +26,7 @@ $.fn.toPx = function(settings){
 		scope: 'body'
 	}, settings);
 	var that = parseFloat(this[0]),
-		scopeTest = jQuery('<div style="display: none; font-size: 1em; margin: 0; padding:0; height: auto; line-height: 1; border:0;">&nbsp;</div>').appendTo(settings.scope),
+		scopeTest = jQuery('<div style="display: none; font-size: 1rem; margin: 0; padding:0; height: auto; line-height: 1; border:0;">&nbsp;</div>').appendTo(settings.scope),
 		scopeVal = scopeTest.height();
 	scopeTest.remove();
 	return Math.round(that * scopeVal) + 'px';
